67535ea0c840c5ee2b98e87fc91e8560c9bc19f3
sferrazzaf <s3francis@gmail.com>
2016-05-24 20:57:59 -0400

    reworked models. destroyed old database. Setting up objects to play with on prop page.

--- house/migrations/0001_initial.py
+++ house/migrations/0001_initial.py
@@ -11,17 +11,57 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Accessory',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('accessory_name', models.CharField(max_length=100)),
+                ('accessory_image', models.ImageField(upload_to='')),
+            ],
+        ),
+        migrations.CreateModel(
             name='Background',
             fields=[
-                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('bg_name', models.CharField(max_length=100)),
                 ('bg_image', models.ImageField(upload_to='')),
             ],
         ),
         migrations.CreateModel(
             name='Doll',
             fields=[
-                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('doll_name', models.CharField(max_length=100)),
                 ('doll_image', models.ImageField(upload_to='')),
             ],
+        ),
+        migrations.CreateModel(
+            name='Dollhouse',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('dollhouse_name', models.CharField(max_length=100)),
+                ('dh_background', models.ForeignKey(to='house.Background')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('username', models.CharField(max_length=100)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='dollhouse',
+            name='user',
+            field=models.ForeignKey(to='house.User'),
+        ),
+        migrations.AddField(
+            model_name='doll',
+            name='dollhouse',
+            field=models.ForeignKey(to='house.Dollhouse'),
+        ),
+        migrations.AddField(
+            model_name='accessory',
+            name='dollhouse',
+            field=models.ForeignKey(to='house.Dollhouse'),
         ),
     ]
--- house/models.py
+++ house/models.py
@@ -1,26 +1,34 @@
 from django.db import models
 
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    def __str__(self):
+        return self.username
+
 class Background(models.Model):
     bg_name = models.CharField(max_length=100)
     bg_image = models.ImageField(max_length=100)
-    bg_user = models.CharField(max_length=100)
-    bg_dollhouse = models.CharField(max_length=100)
     def __str__(self):
         return self.bg_name
+ 
+class Dollhouse(models.Model):
+    dollhouse_name = models.CharField(max_length=100)
+    user = models.ForeignKey(User)
+    dh_background = models.ForeignKey(Background)
+    def __str__(self):
+        return self.dollhouse_name
 
 class Doll(models.Model):
     doll_name = models.CharField(max_length=100)
     doll_image = models.ImageField(max_length=100)
-    doll_user = models.CharField(max_length=100)
-    doll_dollhouse = models.CharField(max_length=100)
+    dollhouse = models.ForeignKey(Dollhouse)
     def __str__(self):
         return self.doll_name
 
 class Accessory(models.Model):
     accessory_name = models.CharField(max_length=100)
     accessory_image = models.ImageField(max_length=100)
-    accessory_user = models.CharField(max_length=100)
-    accessory_dollhouse = models.CharField(max_length=100)
+    dollhouse = models.ForeignKey(Dollhouse)
     def __str__(self):
         return self.accessory_name
 
